5bea3b6f959dc186e70b1fce73b39311eacb1b5d
sepan mohammed <sepanmustafa@gmail.com>
2023-10-02 13:34:51 -0500

    slipt the screen in to proper portions

---
+++ script.js
@@ -0,0 +1,9 @@
+var searchBtn = document.querySelector(".Search-btn");
+
+
+function searchSection () {
+    window.location.replace('./results.html')
+}
+
+
+searchBtn.addEventListener('click', searchSection);
